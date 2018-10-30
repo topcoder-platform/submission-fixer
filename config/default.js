3458538c3c51ece2be5de5209d564c4b19b16520
CWD <cwd@cloudspokes.com>
2018-10-30 11:47:08 -0400

    sorta working good. dynamo is being weird

--- config/default.js
+++ config/default.js
@@ -2,5 +2,12 @@ module.exports = {
   aws: {
     REGION: 'us-east-1'
   },
-  LOG_LEVEL: 'debug'
+  LOG_LEVEL: 'debug',
+  AUTH0_URL: process.env.AUTH0_URL || '',
+  AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE || '',
+  TOKEN_CACHE_TIME: process.env.TOKEN_CACHE_TIME || '',
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID || '',
+  AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET || '',
+  BUS_URL: process.env.BUS_URL || '',
+  SUBS_URL: process.env.SUBS_URL || ''
 }
--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const fs = require('fs')
 const _ = require('lodash')
 const config = require('config')
+const moment = require('moment')
   // const bluebird = require('bluebird')
 const AWS = require('aws-sdk')
 const AmazonS3URI = require('amazon-s3-uri')
+const s3ToURL = require('s3-public-url')
 const parse = require('csv-parse')
-
+const rp = require('request-promise-native')
+const m2mAuth = require('tc-core-library-js').auth.m2m
+const m2m = m2mAuth(_.pick(config, ['AUTH0_URL', 'AUTH0_AUDIENCE', 'TOKEN_CACHE_TIME']))
 
 const s3 = new AWS.S3()
   // const s3p = bluebird.promisifyAll(s3)
 AWS.config.region = config.get('aws.REGION')
 
-// const s3 = bluebird.promisifyAll(s3)
-const m2mAuth = require('tc-core-library-js').auth.m2m
-const m2m = m2mAuth(_.pick(config, ['AUTH0_URL', 'AUTH0_AUDIENCE', 'TOKEN_CACHE_TIME']))
 
 // Database instance mapping
 const dbs = {}
@@ -21,6 +22,7 @@ const dbs = {}
 const dbClients = {}
 
 function getM2Mtoken() {
+  console.log('*** getting M2M token')
   return m2m.getMachineToken(config.AUTH0_CLIENT_ID, config.AUTH0_CLIENT_SECRET)
 }
 
@@ -54,6 +56,7 @@ async function getRecord(legacySubmissionId) {
     dbClient.scan(params, (err, data) => {
       //  console.log('came back from dynamo with: ', err, data)
       if (err) {
+        console.log(err)
         reject(err)
       }
       resolve(data)
@@ -61,26 +64,62 @@ async function getRecord(legacySubmissionId) {
   })
 }
 
-async function downloadFile(fileURL) {
-  let downloadedFile
-  if (/.*amazonaws.*/.test(fileURL)) {
-    const {
-      bucket, key
-    } = AmazonS3URI(fileURL)
-    console.log(`downloadFile(): file is on S3 ${bucket} / ${key}`)
-    downloadedFile = await s3.getObject({
-      Bucket: bucket,
-      Key: key
-    })
-    console.log('downloadedFile: ', downloadedFile)
-    return downloadedFile.Body
-  } else {
-    throw Exception('not an S3 URL')
+async function moveFile(sourceBucket, sourceKey, targetBucket, targetKey) {
+  console.log(`*** moving file ${sourceBucket}/${sourceKey} to ${targetBucket}/${targetKey}`)
+  await s3.copyObject({
+    Bucket: targetBucket,
+    CopySource: `/${sourceBucket}/${sourceKey}`,
+    Key: targetKey
+  })
+  console.log(`*** deleting file ${sourceBucket}/${sourceKey}`)
+  return await s3.deleteObject({
+    Bucket: sourceBucket,
+    Key: sourceKey
+  })
+}
+
+async function postEvent(payload) {
+  const token = await getM2Mtoken()
+  const opts = {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    },
+    uri: config.BUS_URL,
+    method: 'POST',
+    json: true,
+    resolveWithFullResponse: true,
+    body: payload
   }
+
+  console.log('sending event to bus ')
+  return await rp(opts).then((data) => {
+    console.log('*** posted event')
+  })
+}
+
+async function updateSubURL(id, url) {
+  const token = await getM2Mtoken()
+  const opts = {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    },
+    uri: `${config.SUBS_URL}/${id}`,
+    method: 'PATCH',
+    json: true,
+    body: {
+      url
+    }
+  }
+
+  console.log(`*** PATCHING the sub with: ${url}`)
+  return await rp(opts).then((data) => {
+    console.log('*** patched the sub')
+  })
 }
 
 async function loadRecords(csvFile) {
   fs.readFile(csvFile, (err, data) => {
+    console.log('parsing CSV')
     parse(data, {
       comment: '#',
       delimiter: '\t'
@@ -88,24 +127,53 @@ async function loadRecords(csvFile) {
       _.forEach(output, async(row) => {
         let legacySubmissionId = row[0]
         let url = row[6]
-        console.log(legacySubmissionId, url)
+        console.log(`${legacySubmissionId} | ${url}`)
 
         try {
           let r = await getRecord(legacySubmissionId)
-          console.log(r)
 
           if (r.Items.length > 0) {
-            let file = await downloadFile(r.Items[0].url)
-            console.log(file)
+            let fileURL = r.Items[0].url
+            let subId = r.Items[0].id
+              //let file = await downloadFile(r.Items[0].url)
+            if (/.*amazonaws.*/.test(fileURL)) {
+              const {
+                bucket, key
+              } = AmazonS3URI(fileURL)
+              let newURL = s3ToURL.getHttps(bucket, `${subId}.zip`)
+
+              console.log(`${legacySubmissionId} | moving file on S3`)
+              await moveFile(bucket, key, bucket, `${subId}.zip`)
+              console.log(`${legacySubmissionId} | updating dynamo w/ new s3 URL`)
+              let subResult = await updateSubURL(subId, newURL)
+              console.log(subResult)
+              console.log(`${legacySubmissionId} | posting a new unscanned event for file`)
+              let eventResult = await postEvent({
+                "topic": "avscan.action.scan",
+                "originator": "lazybaer-subfixer",
+                "timestamp": moment(new Date(), moment.ISO_8601),
+                "mime-type": "application/json",
+                "payload": {
+                  "status": "unscanned",
+                  "submissionId": subId,
+                  "url": newURL,
+                  "fileName": `${subId}.zip`
+                }
+              })
+              console.log(eventResult)
+            } else {
+              console.log(`${legacySubmissionId} | file isn't on s3: ${fileURL}`)
+            }
           } else {
-            console.log(`could not find recods for legacySubmissionId ${legacySubmissionId}`)
+            console.log(`${legacySubmissionId} | could not find records for legacySubmissionId`)
           }
 
         } catch (e) {
           console.log(e)
         }
 
-        console.log('---')
+        console.log(`${legacySubmissionId} | finished processing line`)
+        console.log('------------------------------------------')
       });
     })
   })
@@ -113,4 +181,4 @@ async function loadRecords(csvFile) {
 }
 
 
-loadRecords('test.csv')
+return loadRecords('test.csv')
